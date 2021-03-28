18b8ac708651d2fb54fb58ac8bc365e4081b1cd6
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2021-03-28 18:17:34 -0400

    Added documentation to config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+# The url of the webpage to run the bot on
 url = "https://www.bestbuy.com/site/searchpage.jsp?st=5900x&_dyncharset=UTF-8&_dynSessConf=&id=pcat17071&type=page&sc=Global&cp=1&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All+Categories&ks=960&keys=keys"
 
+# Sound file must be .wav and in the same directory as the file.
 sound = "123.wav"
 
 # Use raw string so that backslashes are not escapes.
+# To find this profile, open FireFox and go to about:profiles. Copy paste the path "Profile: default-release / Root Directory"
 profile = r'C:\Users\Primary\AppData\Roaming\Mozilla\Firefox\Profiles\wn6904uv.default-release'
